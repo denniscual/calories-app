0a76f5e03bd770ea7716e0e29997c6e2e0d50ea1
Dennis Cual <mamayandpapay@gmail.com>
2022-02-27 23:52:02 +0800

    add comments

--- app/src/features/App.tsx
+++ app/src/features/App.tsx
@@ -7,6 +7,11 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import AppProviders from './AppProviders'
 
+// TODO:
+// - refactor our AuthContext. Check bulletproof-react.
+// - add tests.
+// - push the repo to Github and setup CI/CD in github.
+// - setup Cypress.
 export default function App() {
   return (
     <StrictMode>
