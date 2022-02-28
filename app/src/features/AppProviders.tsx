533e24c0f71e37f3691015fb3c4c11152b383e1f
Dennis Cual <mamayandpapay@gmail.com>
2022-02-28 21:35:46 +0800

    move `theme` inside the components dir

--- app/src/features/AppProviders.tsx
+++ app/src/features/AppProviders.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { BrowserRouter } from 'react-router-dom'
-import theme from 'theme'
 import { QueryClientProvider } from 'react-query'
 import { AuthContextProvider, queryClient } from 'api'
 import CssBaseline from '@mui/material/CssBaseline'
 import AdapterMoment from '@mui/lab/AdapterMoment'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import { DateContextProvider } from './DateContext'
+import { appTheme } from 'components'
 
 const AppProviders: FC = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={appTheme}>
         <CssBaseline />
         <LocalizationProvider dateAdapter={AdapterMoment}>
           <BrowserRouter>
